f129066a7821cedaefb717054f71a1e9e1107733
Emre USTA <emre.usta@stu.fsm.edu.tr>
2020-12-17 13:55:58 +0300

    Updated

---
+++ DarkTema/app/src/main/java/com/example/darktema/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.darktema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.darktema.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNormal.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+        }
+
+        binding.buttonKaranlik.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        }
+    }
+}
